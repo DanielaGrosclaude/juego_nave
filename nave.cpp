2cf7d60434d5c8675375ad877536ea7661db4827
Daniela Grosclaude <86734288+DanielaGrosclaude@users.noreply.github.com>
2022-04-22 20:10:04 -0300

    Update nave.cpp
    Creacion de entorno de conflicto

--- nave.cpp
+++ nave.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
+#include <stdlib.h>
+
 
 #define ARRIBA  72
 #define IZQUIERDA 75
@@ -59,15 +61,17 @@ void pintar_limites (){
 class NAVE{
     int x,y;
     int corazones;
+    int vidas;
 
 
 public:
-    NAVE (int _x, int _y, int _corazones): x(_x), y(_y), corazones (_corazones){}
+    NAVE (int _x, int _y, int _corazones, int _vidas): x(_x), y(_y), corazones (_corazones), vidas (_vidas){}
 
     void pintar();
     void borrar();
     void mover();
     void pintar_corazones();
+    void morir();
 
 
 };
@@ -111,25 +115,81 @@ void NAVE::pintar_corazones(){
     }
 }
 
+//Creo  NAVE 
 
+void NAVE::morir(){
+    if (corazones == 0) {
+        borrar();
+        gotoxy (x,y); printf("   **   ");
+        gotoxy(x,y+1); printf("  ****  ");
+        gotoxy(x,y+2); printf("   **   ");
+        Sleep(200);
+
+        borrar();
+        gotoxy(x,y); printf(" * ** * ");
+        gotoxy(x,y+1); printf("  ****  ");
+        gotoxy(x,y+2); printf(" * ** * ");
+        Sleep(200);
+
+        borrar();
+
+
+        vidas--;
+        corazones = 3;
+        pintar_corazones();
+        pintar();
+
+
+    }
+
+}
+//creo la clase asteroide (enemigo)
+class AST {
+    int x,y;
+public:
+    AST(int _x, int _y):x(_x),y(_y){}
+
+    void pintar();
+    void mover();
+
+};
+
+void AST::pintar(){
+    gotoxy(x,y);printf("%c", 184);
+
+}
+
+void AST::mover(){
+    gotoxy(x,y); printf(" ");
+    y++;
+    if (y>32){
+        x = rand ()%71 + 4;
+        y = 4;
+
+    }
+    pintar();
+
+
+}
 int main() {
 
 
     OcultarCursor();
     pintar_limites();
-
-    NAVE N(7,7,3);
+    
+    NAVE N(7,7,3,3);
     N.pintar();
+    N.pintar_corazones();
 
-    N.mover();
+    AST ast(10,4);
+
 
     bool game_over = false;
     while (!game_over){
-
-
-
+        
+        ast.mover();
+        N.morir();
         N.mover();
-
         Sleep(30);
 
     }
